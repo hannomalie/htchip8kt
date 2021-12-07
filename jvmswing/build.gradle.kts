6a24cefb6ffbb4b06cfad3fafc2677d595361eae
Hannes Pernpeintner <hannes.pernpeintner@gmx.de>
2021-12-07 23:41:01 +0100

    graalvm native image stuff

--- jvmswing/build.gradle.kts
+++ jvmswing/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm")
     application
+    id("com.palantir.graal") version "0.10.0"
 }
 
 repositories {
@@ -14,4 +15,10 @@ application {
     mainClass.set("SwingMain")
 }
 
+graal {
+    mainClass("SwingMain")
+    outputName("htchip8kt")
+    javaVersion("11")
+    windowsVsVarsPath("C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\bin\\amd64\\vcvars64.bat")
+}
 tasks.assembleDist { dependsOn(rootProject.tasks.getByName("build")) }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 rootProject.name = "htchip8kt"
 
 include("jvmswing")
